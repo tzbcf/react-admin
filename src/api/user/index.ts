00dc11efd6316602ffe547379c4e9ce00c4178f0
chenzq <chenzq@kltb.com.cn>
2021-07-05 18:46:40 +0800

    增加mock

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,7 +2,7 @@ import {post} from 'src/api/http';
 import {Login} from './types.d';
 class User{
   async login(data: Login) {
-    return await post('/login', data);
+    return await post('/mock', data);
   }
 }
 
